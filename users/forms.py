c07f6dfe8f68a48eebc0dc0eefe523f021ce4001
vaniakazeko <kazeko.ivan.00@gmail.com>
2024-04-04 10:11:51 +0300

    data validation

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,9 @@
 from django import forms
+from django.contrib.auth import password_validation
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 
 
 class LoginForm(forms.Form):
@@ -16,3 +19,27 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'firstname', 'lastname', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Пользователь с таким адресом электронной почты уже существует.")
+        try:
+            validate_email(email)
+        except ValidationError:
+            raise forms.ValidationError("Некорректный адрес электронной почты.")
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("Пользователь с таким именем уже существует.")
+        return username
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get("password1")
+        try:
+            password_validation.validate_password(password1, self.instance)
+        except ValidationError as error:
+            self.add_error('password1', error)
+        return password1
